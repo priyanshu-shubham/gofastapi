576e66fc442415e16c250f72218ea1262afc78e8
Priyanshu Shubham <priyanshushubham27@gmail.com>
2025-09-15 00:36:45 +0530

    Add byte type

--- openapi.go
+++ openapi.go
@@ -437,6 +437,9 @@ func (b *OpenAPIBuilder) createSchemaFromType(t reflect.Type, validateTag string
 		if t.String() == "uuid.UUID" {
 			schema.Type = "string"
 			schema.Format = "uuid"
+		} else if t.String() == "[]byte" {
+			schema.Type = "string"
+			schema.Format = "byte"
 		} else {
 			schema.Type = "array"
 			schema.Items = b.createSchemaFromType(t.Elem(), "")
